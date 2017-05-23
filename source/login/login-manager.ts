bcd111ef25b57b991bf0f2bd9a577ccda5dcaaa2
David Daniel <davedan7@gmail.com>
2017-05-23 14:27:26 -0600

    Tabs to spacse

--- source/login/login-manager.ts
+++ source/login/login-manager.ts
@@ -1,11 +1,13 @@
 export class LoginManager {
-    constructor (owner) {
-        this.stateOwner = owner
-    }
-    handleChange = (e) => {
-        const target = e.target
-        this.stateOwner.setState({
-            [target.name]: target.value
-        })
-    }
+  stateOwner
+  constructor(owner) {
+    this.stateOwner = owner
+  }
+
+  handleChange = (e) => {
+    const target = e.target
+    this.stateOwner.setState({
+      [target.name]: target.value
+    })
+  }
 }
